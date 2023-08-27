0fbc5f520668d4dc99ce319cf39ca0c8a79de523
Aina Rass <hei.aina.59@gmail.com>
2023-08-27 15:04:27 +0100

    fix: transform to pdf

--- src/main/resources/db/migration/V4__Insert_Hei_Employee.sql
+++ src/main/resources/db/migration/V4__Insert_Hei_Employee.sql
@@ -1,4 +1,4 @@
 ---- V4__Insert_Hei_Employee.sql
 
 INSERT INTO entreprise ( nom, description, email_contact)
-VALUES ('hei employee', 'gestion d''employee', 'manager@gmail.com');
+VALUES ('hei employee', 'gestion d''employee', 'hei.school@gmail.com');
